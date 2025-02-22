14ba7551bcc1ec6df81214ba1c5f0c9caec57547
Wu Feiyang <35396618+FeiyangWuPK@users.noreply.github.com>
2025-02-22 15:31:16 -0500

    Integrate conditional unet 1d from github

--- latent_diffusion_policy/model/__init__.py
+++ latent_diffusion_policy/model/__init__.py
@@ -1,3 +1,28 @@
 from .ldf import EnvRunner, LatentDiffusionPolicyWithFiLM
+from typing import TYPE_CHECKING
+
+from diffusers.utils import (
+    DIFFUSERS_SLOW_IMPORT,
+    _LazyModule,
+    is_flax_available,
+    is_torch_available,
+)
+
+
+_import_structure = {}
+
+if is_torch_available():
+    _import_structure["unets.unet_1d_condition"] = ["UNet1DConditionModel"]
+
+
+if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
+    from .unets import UNet1DConditionModel
+
+else:
+    import sys
+
+    sys.modules[__name__] = _LazyModule(
+        __name__, globals()["__file__"], _import_structure, module_spec=__spec__
+    )
 
 __all__ = ["EnvRunner", "LatentDiffusionPolicyWithFiLM"]
